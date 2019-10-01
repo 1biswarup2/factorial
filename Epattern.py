e023ff06c3ea618b2293fd9f004f84d1a304d151
paras-aghija <56034167+paras-aghija@users.noreply.github.com>
2019-10-01 23:13:45 +0530

    Update Epattern.py

--- Epattern.py
+++ Epattern.py
@@ -1,12 +1,12 @@
 for i in range(7):
      if i==0 or i==6:
         for j in range(4):
-            print("*",end="")
+            print("*", end="")
    
 
      elif i==3:
         for j in range(i):
-            print("*",end="")
+            print("*", end="")
      else:
-        print("*",end="")
-     print("\n")
+        print("*", end="")
+     print()
